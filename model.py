d46749532c0de5315d1b7a4b5c6452b3884f5aed
Szymon Rosłaniec <s22729@pjwstk.edu.pl>
2023-10-10 11:32:32 +0200

    val split, save to ./models

--- model.py
+++ model.py
@@ -4,17 +4,21 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 import pickle
 
 
-def lgr_model(X_train, X_test, y_train, y_test):
+def lgr_model(X_train, X_test, y_train, y_test,X_val, y_val):
     logmodel = LogisticRegression(max_iter=500)
     logmodel.fit(X_train,y_train)
+    y_pred_val.predict(X_val)
     y_pred=logmodel.predict(X_test)
     print(classification_report(y_test,y_pred))
-    pickle.dump(logmodel, open("lgr.pkl", "wb"))
+    print(classification_report(y_val,y_pred_val))
+    pickle.dump(logmodel, open("./models/lgr.pkl", "wb"))
 
 
-def knn_model(X_train, X_test, y_train, y_test,k):
-    knn_model = KNeighborsClassifier(n_neighbors=k)
+def knn_model(X_train, X_test, y_train, y_test, X_val, y_val):
+    knn_model = KNeighborsClassifier(n_neighbors=8)
     knn_model.fit(X_train, y_train)
+    y_pred_knn_val = knn_model.predict(X_val)
     y_pred_knn = knn_model.predict(X_test)
     print(accuracy_score(y_test, y_pred_knn))
-    pickle.dump(knn_model, open("knn.pkl", "wb"))
+    print(accuracy_score(y_val, y_pred_knn_val))
+    pickle.dump(knn_model, open("./models/knn.pkl", "wb"))
--- preprocess.py
+++ preprocess.py
@@ -2,10 +2,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def preprocess(df):
-    df = df.apply(lambda x: pd.factorize(x)[0])
     if df.duplicated().sum() > 0:
-        df.rop_duplicates
+        df.drop_duplicates()
+    df = df.apply(lambda x: pd.factorize(x)[0])
     X = df.drop(columns='class')
     y = df['class']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, shuffle=True)
-    return X_train, X_test, y_train, y_test
+    X_train, X_val, y_train, y_val = train_test_split(X_train,y_train,test_size=0.25, random_state=31, shuffle=True)
+    return X_train, X_test, y_train, y_test, X_val, y_val
