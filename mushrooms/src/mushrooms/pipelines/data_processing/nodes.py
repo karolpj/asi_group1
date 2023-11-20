0c7da30d477191ed848801e473b52aa30b06d1b4
szymek <szymon.roslaniecc@gmail.com>
2023-11-20 22:52:44 +0100

    added fastapi and db

--- mushrooms/src/mushrooms/pipelines/data_processing/nodes.py
+++ mushrooms/src/mushrooms/pipelines/data_processing/nodes.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import sqlite3
 
 
+
+def load_mushrooms() -> pd.DataFrame:
+    database_file_path = '../data/mushrooms.db'
+    connection = sqlite3.connect(database_file_path) 
+    df = pd.read_sql(f'SELECT * FROM mushrooms', connection)
+    connection.close()
+    return df
 
 def preprocess_mushrooms(mushrooms: pd.DataFrame) -> pd.DataFrame:
     onehot = pd.get_dummies(mushrooms,dtype=int)
--- mushrooms/src/mushrooms/pipelines/data_processing/pipeline.py
+++ mushrooms/src/mushrooms/pipelines/data_processing/pipeline.py
@@ -1,11 +1,17 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import create_model_input_table, preprocess_mushrooms
+from .nodes import create_model_input_table, preprocess_mushrooms, load_mushrooms
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
+            node(
+                func=load_mushrooms,
+                inputs=None,
+                outputs="mushrooms",
+                name="load_mushrooms_node",
+            ),
             node(
                 func=preprocess_mushrooms,
                 inputs="mushrooms",
